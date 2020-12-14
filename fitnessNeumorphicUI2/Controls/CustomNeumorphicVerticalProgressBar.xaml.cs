33dacc3deb2482606414545666f8486157d8b3c4
abdur mohammed <abdur.mohammad@xam-consulting.com>
2020-12-15 00:12:56 +1100

    Initial commit

---
+++ fitnessNeumorphicUI2/Controls/CustomNeumorphicVerticalProgressBar.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace fitnessNeumorphicUI2.Controls
+{
+    public partial class CustomNeumorphicVerticalProgressBar : ContentView
+    {
+        #region fields
+        public static readonly BindableProperty ProgressTypeLabelProperty =
+              BindableProperty.CreateAttached(
+              "ProgressTypeLabel",
+              typeof(string),
+              typeof(NeumorphicSquare),
+              string.Empty,
+              propertyChanged: ProgressTypeLabelChanged);
+
+        public static readonly BindableProperty ProgressPercentageProperty =
+               BindableProperty.CreateAttached(
+              "ProgressPercentage",
+              typeof(string),
+              typeof(NeumorphicSquare),
+              string.Empty,
+              propertyChanged: ProgressPercentageChanged);
+
+        public static readonly BindableProperty ProgressBarColorProperty =
+             BindableProperty.CreateAttached(
+            "ProgressBarColor",
+            typeof(Color),
+            typeof(NeumorphicSquare),
+            Color.CornflowerBlue,
+            propertyChanged: ProgressBarColorChanged);
+
+        private static int _fullProgressBarHeight = 250;
+        #endregion
+
+        #region Properties
+        public static string GetProgressTypeLabel(BindableObject view)
+        {
+            return (string)view.GetValue(ProgressTypeLabelProperty);
+        }
+
+        public static void SetProgressTypeLabel(BindableObject view, string value)
+        {
+            view.SetValue(ProgressTypeLabelProperty, value);
+        }
+
+        public static string GetProgressPercentage(BindableObject view)
+        {
+            return (string)view.GetValue(ProgressPercentageProperty);
+        }
+
+        public static void SetProgressPercentage(BindableObject view, string value)
+        {
+            view.SetValue(ProgressPercentageProperty, value);
+        }
+
+        public static Color GetProgressBarColor(BindableObject view)
+        {
+            return (Color)view.GetValue(ProgressBarColorProperty);
+        }
+
+        public static void SetProgressBarColor(BindableObject view, string value)
+        {
+            view.SetValue(ProgressBarColorProperty, value);
+        }
+        #endregion
+        #region constructor
+        public CustomNeumorphicVerticalProgressBar()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Methods
+
+        private static void ProgressTypeLabelChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var targetType = bindable as CustomNeumorphicVerticalProgressBar;
+
+            if(targetType != null)
+            {
+                targetType.TypeLabel.Text = GetProgressTypeLabel(bindable);
+            }
+        }
+
+        private static void ProgressPercentageChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var targetType = bindable as CustomNeumorphicVerticalProgressBar;
+            var progressPercentage = GetProgressPercentage(bindable);
+            
+            if (targetType != null)
+            {
+                int progressBarInt = 0;
+
+                targetType.ProgressPercentage.Text = $"{progressPercentage}%";
+
+                Int32.TryParse(progressPercentage, out progressBarInt);
+
+                progressBarInt = decimal.ToInt32((((decimal)progressBarInt / 100) * _fullProgressBarHeight));
+
+                targetType.ProgressColorBar.HeightRequest = progressBarInt;
+            }
+        }
+
+
+        private static void ProgressBarColorChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var targetType = bindable as CustomNeumorphicVerticalProgressBar;
+
+            if (targetType != null)
+            {
+                targetType.ProgressColorBar.BackgroundColor = GetProgressBarColor(bindable);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ fitnessNeumorphicUI2/Controls/NeumorphicSquare.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.NeoControls;
+
+namespace fitnessNeumorphicUI2.Controls
+{
+    public partial class NeumorphicSquare : NeoButton
+    {
+        public static readonly BindableProperty CustomImageSourceProperty =
+           BindableProperty.CreateAttached(
+               "CustomImageSource",
+               typeof(string),
+               typeof(NeumorphicSquare),
+               string.Empty,
+               propertyChanged: CustomImageSourceChanged);
+
+        public static string GetCustomImageSource(BindableObject view)
+        {
+            return (string)view.GetValue(CustomImageSourceProperty);
+        }
+
+        public static void SetCustomImageSource(BindableObject view, string value)
+        {
+            view.SetValue(CustomImageSourceProperty, value);
+        }
+
+        private static void CustomImageSourceChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var targetView = bindable as NeumorphicSquare;
+            targetView.InternalImageView.Source = GetCustomImageSource(bindable);
+        }
+
+        public NeumorphicSquare()
+        {
+            InitializeComponent();
+        }
+
+        private void NeoButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            neo_button.ShadowDrawMode = neo_button.ShadowDrawMode == ShadowDrawMode.All ? ShadowDrawMode.OuterOnly: ShadowDrawMode.All;
+        }
+    }
+}
